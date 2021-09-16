3d999190c0c6d7293dfcd26d47647dcbea8b233b
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-09-16 23:37:37 +0900

    Add(jiwon/signup): 회원가입 API 구현 #5 UserStorage.js: 회원가입을 위한 insert문 작성 User.js: query문 결과로 return 값 적용

---
+++ app/src/models/services/User/User.js
@@ -0,0 +1,69 @@
+import UserStorage from "./UserStorage";
+import MajorStorage from "../Major/MajorStorage";
+import Cryptor from "../../utils/Cryptor";
+
+class User {
+  constructor(req) {
+    this.body = req.body;
+  }
+
+  async signup() {
+    const user = this.body;
+    try {
+      const number = {};
+      let schoolNum, majorNum, detailMajorNum, departmentNum;
+      const regionNum = await MajorStorage.findRegionNumByName(user.region);
+      if (regionNum) {
+        schoolNum = await MajorStorage.findSchoolNumByName(user.school);
+        if (!schoolNum)
+          schoolNum = await MajorStorage.createSchoolByName(
+            regionNum,
+            user.school,
+          );
+      }
+      departmentNum = await MajorStorage.findDepartmentNumByName(
+        user.department,
+      );
+      if (!departmentNum)
+        departmentNum = await MajorStorage.createDepartmentByName(
+          user.department,
+        );
+      if (departmentNum)
+        majorNum = await MajorStorage.findMajorNumByName(user.major);
+      if (!majorNum)
+        majorNum = await MajorStorage.createMajorByName(
+          departmentNum,
+          user.major,
+        );
+      detailMajorNum = await MajorStorage.findDetailMajorNumByName(
+        user.detailMajor,
+      );
+      if (!detailMajorNum)
+        detailMajorNum = await MajorStorage.createDetailMajorByName(
+          majorNum,
+          user.detailMajor,
+        );
+      number.school = schoolNum;
+      number.deparment = departmentNum;
+      number.major = majorNum;
+      number.detailMajor = detailMajorNum;
+
+      const { hash, salt } = await Cryptor.encrypt(user.psword);
+      user.psword = hash;
+      user.salt = salt;
+
+      const signup = await UserStorage.signup(user, number);
+      if (signup)
+        return { success: true, msg: "회원가입이 정상 처리 되었습니다." };
+      return { success: false, msg: "회원가입 실패, 개발자에게 문의해주세요" };
+    } catch (err) {
+      if (err.sqlMessage.includes("email"))
+        return { success: false, msg: "이메일이 중복되었습니다." };
+      if (err.sqlMessage.includes("nickname"))
+        return { success: false, msg: "닉네임이 중복되었습니다." };
+      return { success: false, msg: err };
+    }
+  }
+}
+
+export default User;
---
+++ app/src/models/services/User/UserStorage.js
@@ -0,0 +1,33 @@
+import mariadb from "../../../config/mariadb";
+
+class UserStorage {
+  static async signup(user, number) {
+    let conn;
+    try {
+      conn = await mariadb.getConnection();
+      const query = `INSERT INTO users(school_no, department_no, major_no, detail_major_no, grade, email, nickname, psword, salt) 
+      VAULES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+
+      const result = await conn.query(query, [
+        number.school,
+        number.department,
+        number.major,
+        number.detailMajor,
+        user.grade,
+        user.email,
+        user.nickname,
+        user.psword,
+        user.salt,
+      ]);
+
+      if (result.affectedRows) return true;
+      return false;
+    } catch (err) {
+      throw err;
+    } finally {
+      conn?.release();
+    }
+  }
+}
+
+export default UserStorage;
