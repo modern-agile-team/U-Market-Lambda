aa3b38856bc46494fb63d6da24abe4f9ff3f1db8
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-09-16 23:32:48 +0900

    Modify(jiwon/signup): env를 사용하기 위한 dotenv와 request에서 body하기 위한 bodyParser 추가

--- app/app.js
+++ app/app.js
@@ -1,6 +1,12 @@
 const express = require("serverless-express/express");
+const dotenv = require("dotenv");
 
 const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/hi", (req, res) => {
   return res.status(200).json({ message: "world" });
