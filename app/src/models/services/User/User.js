273741352e61bc38e16cab308a089331a3726427
kimjiwon-98 <jiwonpg98@gmail.com>
2021-09-25 02:29:19 +0900

    Modify(jiwon/signup): db 연결 끊음 상태 변경

--- app/src/models/services/User/User.js
+++ app/src/models/services/User/User.js
@@ -21,10 +21,10 @@ class User {
         return { success: true, msg: "회원가입이 정상 처리 되었습니다." };
       return { success: false, msg: "회원가입 실패, 개발자에게 문의해주세요" };
     } catch (err) {
-      // if (err.sqlMessage.includes("email"))
-      //   return { success: false, msg: "이메일이 중복되었습니다." };
-      // if (err.sqlMessage.includes("nickname"))
-      //   return { success: false, msg: "닉네임이 중복되었습니다." };
+      if (err.text.includes("email_UNIQUE"))
+        return { success: false, msg: "이메일이 중복되었습니다." };
+      if (err.text.includes("nickname_UNIQUE"))
+        return { success: false, msg: "닉네임이 중복되었습니다." };
       return { success: false, msg: err };
     }
   }
@@ -32,41 +32,45 @@ class User {
   async findNumByRequest(user) {
     let number = {};
     let schoolNum, majorNum, detailMajorNum, departmentNum;
-    const regionNum = await MajorStorage.findRegionNumByName(user.region);
-    if (regionNum) {
-      schoolNum = await MajorStorage.findSchoolNumByName(user.school);
-      // if (!schoolNum)
-      //   schoolNum = await MajorStorage.createSchoolByName(
-      //     regionNum,
-      //     user.school,
-      //   );
-    }
-    departmentNum = await MajorStorage.findDepartmentNumByName(user.department);
-    // if (!departmentNum)
-    //   departmentNum = await MajorStorage.createDepartmentByName(
-    //     user.department,
-    //   );
-    if (departmentNum)
-      majorNum = await MajorStorage.findMajorNumByName(user.major);
-    // if (!majorNum)
-    //   majorNum = await MajorStorage.createMajorByName(
-    //     departmentNum,
-    //     user.major,
-    //   );
-    detailMajorNum = await MajorStorage.findDetailMajorNumByName(
-      user.detailMajor,
-    );
-    // if (!detailMajorNum)
-    //   detailMajorNum = await MajorStorage.createDetailMajorByName(
-    //     majorNum,
-    //     user.detailMajor,
-    //   );
-    number.school = schoolNum;
-    number.department = departmentNum;
-    number.major = majorNum;
-    number.detailMajor = detailMajorNum;
+    try {
+      const regionNum = await MajorStorage.findRegionNumByName(user.region);
+      if (regionNum) {
+        schoolNum = await MajorStorage.findSchoolNumByName(user.school);
+        if (!schoolNum)
+          schoolNum = await MajorStorage.createSchoolByName(
+            regionNum,
+            user.school,
+          );
+      }
+      departmentNum = await MajorStorage.findDepartmentNumByName(
+        user.department,
+      );
+      if (!departmentNum)
+        departmentNum = await MajorStorage.createDepartmentByName(
+          user.department,
+        );
+      if (departmentNum)
+        majorNum = await MajorStorage.findMajorNumByName(user.major);
+      if (!majorNum)
+        majorNum = await MajorStorage.createMajorByName(
+          departmentNum,
+          user.major,
+        );
+      detailMajorNum = await MajorStorage.findDetailMajorNumByName(
+        user.detailMajor,
+      );
+      if (!detailMajorNum)
+        detailMajorNum = await MajorStorage.createDetailMajorByName(
+          majorNum,
+          user.detailMajor,
+        );
+      number.school = schoolNum;
+      number.detailMajor = detailMajorNum;
 
-    return number;
+      return number;
+    } catch (err) {
+      return { success: false, msg: err };
+    }
   }
 }
 
--- app/src/models/services/User/UserStorage.js
+++ app/src/models/services/User/UserStorage.js
@@ -1,17 +1,15 @@
-const mariadb = require("../../../config/mariadb");
+const mariadb = require("../../../config/mysql");
 
 class UserStorage {
   static async signup(user, number) {
     let conn;
     try {
       conn = await mariadb.getConnection();
-      const query = `INSERT INTO users(school_no, department_no, major_no, detail_major_no, grade, email, nickname, psword, salt) 
-      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+      const query = `INSERT INTO users(school_no, detail_major_no, grade, email, nickname, psword, salt) 
+      VALUES (?, ?, ?, ?, ?, ?, ?)`;
 
       const result = await conn.query(query, [
         number.school,
-        number.department,
-        number.major,
         number.detailMajor,
         user.grade,
         user.email,
@@ -19,13 +17,12 @@ class UserStorage {
         user.psword,
         user.salt,
       ]);
-
       if (result.affectedRows) return true;
       return false;
     } catch (err) {
       throw err;
     } finally {
-      conn?.end();
+      await conn?.release();
     }
   }
 }
