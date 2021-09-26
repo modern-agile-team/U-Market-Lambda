ef13879562199ae613ba7dcddc5f4b293fd9795c
jiwonpg98 <jiwonpg98@gmail.com>
2021-09-26 23:28:02 +0900

    Modify(jiwon/signup): db 모듈 변경

--- app/src/config/mysql.js
+++ app/src/config/mysql.js
@@ -1,12 +1,11 @@
-const db = require("serverless-mysql");
-
-const mysql = db({
+const mysql = require("serverless-mysql")({
   config: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     port: 3306,
+    connectTimeout: 500,
   },
 });
 
