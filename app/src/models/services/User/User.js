1425807ed84c13864040b8f12e117cd8bb67bc20
kimjiwon-98 <jiwonpg98@gmail.com>
2021-09-28 20:19:37 +0900

    Refactor(jiwon/signup): 회원가입 시 필요한 데이터 추가 및 에러처리 변경 #5

--- app/src/models/services/User/User.js
+++ app/src/models/services/User/User.js
@@ -12,15 +12,14 @@ class User {
       const { hash, salt } = await Cryptor.encrypt(user.psword);
       user.psword = hash;
       user.salt = salt;
-
       const signup = await UserStorage.signup(user);
       if (signup)
         return { success: true, msg: "회원가입이 정상 처리 되었습니다." };
       return { success: false, msg: "회원가입 실패, 개발자에게 문의해주세요" };
     } catch (err) {
-      if (err.text.includes("email_UNIQUE"))
+      if (err.sqlMessage.includes("email_UNIQUE"))
         return { success: false, msg: "이메일이 중복되었습니다." };
-      if (err.text.includes("nickname_UNIQUE"))
+      if (err.sqlMessage.includes("nickname_UNIQUE"))
         return { success: false, msg: "닉네임이 중복되었습니다." };
       return { success: false, msg: err };
     }
--- app/src/models/services/User/UserStorage.js
+++ app/src/models/services/User/UserStorage.js
@@ -4,18 +4,19 @@ class UserStorage {
   static async signup(user) {
     try {
       await mysql.connect();
-      const query = `INSERT INTO users(school_no, detail_major_no, grade, email, nickname, psword, salt) 
+      const query = `INSERT INTO users(school_no, major_no, grade, email, nickname, psword, salt) 
       VALUES (?, ?, ?, ?, ?, ?, ?)`;
 
       const result = await mysql.query(query, [
         user.schoolNum,
-        user.detailMajorNum,
+        user.majorNum,
         user.grade,
         user.email,
         user.nickname,
         user.psword,
         user.salt,
       ]);
+
       if (result.affectedRows) return true;
       return false;
     } catch (err) {
