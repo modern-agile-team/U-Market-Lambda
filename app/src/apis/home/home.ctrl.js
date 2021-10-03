b196c1b97a2ad3d3a60910ce0e90e2ad6cbd2310
woorim960 <woorimprog@gmail.com>
2021-10-03 22:43:55 +0900

    Add(woorim/homeAPI): 메인화면 가격별조회의 400 에러 응답 추가 #8

--- app/src/apis/home/home.ctrl.js
+++ app/src/apis/home/home.ctrl.js
@@ -22,14 +22,38 @@ const home = {
   },
   byPrice: async (req, res) => {
     try {
+      let response = {};
+
+      const params = req.params;
+      if (!["asc", "desc"].includes(params.sort)) {
+        response = getError400(
+          "GET /api/home/by-price/:sort/:startNo/:limit",
+          "잘못된 요청입니다. sort 파라미터는 'asc' 혹은 'desc' 둘 중 하나만 가능합니다.",
+        );
+        return res.status(400).json(response);
+      } else if (isNaN(params.startNo)) {
+        response = getError400(
+          "GET /api/home/by-price/:sort/:startNo/:limit",
+          "잘못된 요청입니다. 'startNo'는 숫자만 가능합니다.",
+        );
+        return res.status(400).json(response);
+      } else if (isNaN(params.limit)) {
+        response = getError400(
+          "GET /api/home/by-price/:sort/:startNo/:limit",
+          "잘못된 요청입니다. 'limit'는 숫자만 가능합니다.",
+        );
+        return res.status(400).json(response);
+      }
+
       const product = new Product(req);
       const products = await product.findAllAboutHomeBasedPrice();
 
-      const response = {
+      response = {
         success: true,
         msg: "가격별 물품 데이터 조회에 성공하셨습니다.",
         products,
       };
+
       logger.info(
         `GET /api/home/by-price/:sort/:startNo/:limit 200 ${response.msg}`,
       );
@@ -41,6 +65,16 @@ const home = {
       return res.status(500).json(err);
     }
   },
+};
+
+const getError400 = (url, errorMsg) => {
+  const response = {
+    success: false,
+    msg: errorMsg,
+  };
+
+  logger.error(`${url} 400 ${response.msg}`);
+  return response;
 };
 
 module.exports = {
