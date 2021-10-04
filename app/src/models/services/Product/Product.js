41053087b82571da94123edf375a501ab84c706a
woorim960 <woorimprog@gmail.com>
2021-10-04 17:51:45 +0900

    Add(woorim/marketApi): 거래장터 메인화면 API 추가 #22

--- app/src/models/services/Product/Product.js
+++ app/src/models/services/Product/Product.js
@@ -5,12 +5,28 @@ class Product {
     this.query = req.query;
     this.params = req.params;
     this.body = req.body;
+    this.sql = req.sql;
   }
 
   async findAllAboutHomeBasedPrice() {
     const { startNo, sort, limit } = this.query;
     const attr = { startNo: Number(startNo), sort, limit: Number(limit) };
     const products = await ProductStorage.findAllBasedPriceBy(attr);
+    return products;
+  }
+
+  async findAllAboutMarketBasedPrice() {
+    const { startNo, startPriceRange, endPriceRange, limit } = this.query;
+    const attr = {
+      startNo: Number(startNo),
+      startPriceRange: Number(startPriceRange),
+      endPriceRange: Number(endPriceRange),
+      limit: Number(limit),
+    };
+    const products = await ProductStorage.findAllAboutMarketBasedPriceBy(
+      attr,
+      this.sql,
+    );
     return products;
   }
 
