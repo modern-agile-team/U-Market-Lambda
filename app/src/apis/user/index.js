d14288745428d39b9721b6f6f7eb2ac3024d8275
jiwonpg98 <jiwonpg98@gmail.com>
2021-10-05 01:53:03 +0900

    Modify(jiwon/refactoring): joi middleware  추가 #4 #5 #14 #20

--- app/src/apis/user/index.js
+++ app/src/apis/user/index.js
@@ -1,9 +1,15 @@
 const express = require("serverless-express/express");
 const ctrl = require("./user.ctrl");
+const joi = require("../../utils/schemas");
+const joiValidator = require("../../middleware/validation");
 
 const router = express.Router();
 
-router.post("/signup", ctrl.signup);
-router.post("/login", ctrl.login);
+router.post(
+  "/signup",
+  joiValidator(joi.signup_POST_schema, "body"),
+  ctrl.signup,
+);
+router.post("/login", joiValidator(joi.login_POST_schema, "body"), ctrl.login);
 
 module.exports = router;
