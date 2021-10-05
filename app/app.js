80190826cfc0329bee9021b960a05bc62ed6dd6c
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-10-05 21:01:54 +0900

    Add(jiwon/refactoring): 람다 오류로 인한 cors 설치 #20

--- app/app.js
+++ app/app.js
@@ -2,21 +2,36 @@ const express = require("serverless-express/express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const logger = require("./src/config/logger");
-const errorMiddleware = require("./src/middleware/error.Middleware");
+const errorMiddleware = require("./src/middleware/errorMiddleware");
+const cors = require("cors");
 
 const app = express();
 
 dotenv.config();
 
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "GET, PUT, POST, PATCH ,DELETE, OPTIONS",
+//   );
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header("Content-Types", "application/json");
+//   next();
+// });
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(
-  morgan("tiny", {
-    stream: {
-      write: message => logger.info(message),
-    },
-  }),
-);
+if (process.env.MODE === "dev") {
+  app.use(
+    morgan("tiny", {
+      stream: {
+        write: message => logger.info(message),
+      },
+    }),
+  );
+}
 app.use(errorMiddleware);
 
 const user = require("./src/apis/user");
