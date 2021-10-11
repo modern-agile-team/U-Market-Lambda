64e1e98bfb3fb3ba96d5ea7c00ce4b6bdb9e7c33
jiwonpg98 <jiwonpg98@gmail.com>
2021-10-12 00:40:45 +0900

    Modify(jiwon/mypageAPI): 구매 목록 API 등록 #35

--- app/app.js
+++ app/app.js
@@ -18,6 +18,7 @@ const communities = require("./src/apis/communities");
 const major = require("./src/apis/major");
 const watchlist = require("./src/apis/watchlist");
 const selllist = require("./src/apis/selllist");
+const buylist = require("./src/apis/buylist");
 
 app.use("/api/user", user);
 app.use("/api/home", home);
@@ -26,6 +27,7 @@ app.use("/api/products", products);
 app.use("/api/communities", communities);
 app.use("/api/watchlist", watchlist);
 app.use("/api/selllist", selllist);
+app.use("/api/buylist", buylist);
 app.use(errorMiddleware);
 
 module.exports = app;
