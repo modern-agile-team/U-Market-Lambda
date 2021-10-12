5b745bb3bcbcfaaff0cae46a903f0668cb10b55b
woorim960 <woorimprog@gmail.com>
2021-10-13 00:54:36 +0900

    Add(woorim/marketAPI): 거래장터 수정 API 경로 추가 #22

--- app/src/apis/products/index.js
+++ app/src/apis/products/index.js
@@ -7,19 +7,26 @@ const router = express.Router();
 
 router.get(
   "/",
-  validation(joi.products.root_GET_schema, "query"),
+  validation(joi.products.get.query.root, "query"),
   ctrl.products.home,
 );
 router.get(
   "/:productNo",
-  validation(joi.products.productNo_GET_schema, "params"),
+  validation(joi.products.get.params.productNo, "params"),
   ctrl.products.detailView,
 );
 
 router.post(
   "/",
-  validation(joi.products.root_POST_schema, "body"),
+  validation(joi.products.post.body.root, "body"),
   ctrl.products.create,
+);
+
+router.put(
+  "/:productNo",
+  validation(joi.products.put.params.productNo, "params"),
+  validation(joi.products.put.body.productNo, "body"),
+  ctrl.products.detailView,
 );
 
 module.exports = router;
