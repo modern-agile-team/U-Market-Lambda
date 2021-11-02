1cd33292f76f0cd292286d037cbed031ee09267e
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-11-02 16:51:10 +0900

    Modify(jiwon/comment): 댓글이 DB부분에서 생성되지 않았을 때 에러처리 #51

--- app/src/middleware/errorMiddleware.js
+++ app/src/middleware/errorMiddleware.js
@@ -45,6 +45,8 @@ function errorMiddleware(err, req, res, next) {
     return res.status(400).json({ msg: "현재 비밀번호와 맞지 않습니다." });
   if (err.message === "Not Exist ImageUrl")
     return res.status(400).json({ msg: "image의 주소가 넘어오지 않았습니다." });
+  if (err.message === "Create Fail Comment")
+    return res.status(400).json({ msg: "댓글이 생성되지 않았습니다." });
 
   return res.status(500).json({ error: err.message });
 }
