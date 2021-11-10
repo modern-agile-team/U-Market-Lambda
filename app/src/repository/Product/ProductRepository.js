469025a461f1c6947d63467719147d2c0e2a872f
jiwonpg98 <jiwonpg98@gmail.com>
2021-11-10 13:58:40 +0900

    Refactor(jiwon/market): products에서 안쓰는 댓글 관련 query문 삭제 #22

--- app/src/repository/Product/ProductRepository.js
+++ app/src/repository/Product/ProductRepository.js
@@ -39,10 +39,8 @@ class ProductRepository {
     try {
       await mysql.connect();
       const query = `
-        SELECT pd.no, title, price, COUNT(p_cmt.no) AS commentCnt, interest_cnt AS interestCnt, thumbnail 
+        SELECT pd.no, title, price, interest_cnt AS interestCnt, thumbnail 
         FROM products AS pd
-        LEFT JOIN product_comments AS p_cmt
-        ON pd.no = p_cmt.product_no
         WHERE pd.in_date > (CURRENT_TIMESTAMP() - INTERVAL 7 DAY)
         GROUP BY pd.no
         ORDER BY hit DESC
@@ -62,9 +60,7 @@ class ProductRepository {
     try {
       await mysql.connect();
       const query = `
-        SELECT pd.no, title, price, COUNT(p_cmt.no) AS commentCnt, interest_cnt AS interestCnt, thumbnail FROM products AS pd
-        LEFT JOIN product_comments AS p_cmt
-        ON pd.no = p_cmt.product_no
+        SELECT pd.no, title, price, interest_cnt AS interestCnt, thumbnail FROM products AS pd
         GROUP BY pd.no
         ORDER BY pd.no DESC
         LIMIT ?;`;
@@ -127,11 +123,9 @@ class ProductRepository {
     try {
       await mysql.connect();
       const query = `
-          SELECT pd.no, title, price, COUNT(p_cmt.no) AS commentCnt, 
+          SELECT pd.no, title, price,
             interest_cnt AS interestCnt, thumbnail 
           FROM products AS pd
-          LEFT JOIN product_comments AS p_cmt
-          ON pd.no = p_cmt.product_no
           WHERE pd.no >= ? AND price >= ? AND price <= ? ${filterSql}
           GROUP BY pd.no
           ORDER BY price ASC, pd.no DESC
