ad3f890862fbce92d2fad0c2f849850f7e825624
jiwonpg98 <jiwonpg98@gmail.com>
2021-11-10 13:59:24 +0900

    Refactor(jiwon/market): watchlist에서 no 컬럼 추가 #31

--- app/src/repository/Watchlist/WatchlistRepository.js
+++ app/src/repository/Watchlist/WatchlistRepository.js
@@ -5,7 +5,7 @@ class WatchlistRepository {
     try {
       await mysql.connect();
 
-      const query = `SELECT pr.title, pr.price, pr.interest_cnt AS likeCnt, pr.thumbnail, pr.trading_status_no AS statusNum, DATE_FORMAT(pr.in_date, "%Y.%m.%d") AS inDate  FROM interest_products AS ip
+      const query = `SELECT pr.no, pr.title, pr.price, pr.interest_cnt AS likeCnt, pr.thumbnail, pr.trading_status_no AS statusNum, DATE_FORMAT(pr.in_date, "%Y.%m.%d") AS inDate  FROM interest_products AS ip
       JOIN products AS pr
       ON pr.no = ip.product_no
        WHERE ip.user_no = ?;`;
